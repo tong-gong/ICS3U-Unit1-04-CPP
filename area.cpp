618f4400ba69e89a2b49798e41b76aba449c10b1
Ubuntu <ubuntu@ip-172-31-42-232.ec2.internal>
2020-11-20 18:46:29 +0000

    commit

---
+++ area.cpp
@@ -0,0 +1,14 @@
+// Copyright(C) 2020 Tong Gong All Right received
+//
+// Created by Tong Gong
+// Created time November 2020
+// This is a program to calculate the area and the perimeter of a rectangle
+
+
+#include <iostream>
+
+
+int main() {
+    std::cout << "4*3=" << (4*3) << std::endl;
+    std::cout << "2*(4+3)=" <<(2*(4+3)) << std::endl;
+}
